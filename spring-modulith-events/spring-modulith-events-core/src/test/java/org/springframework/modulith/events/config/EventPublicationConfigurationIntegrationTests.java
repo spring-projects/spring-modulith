spring-projects/spring-modulith

commit d9e761e50b90c22c7b56298cd8a9bcbaa27e9c51

GH-149 - Polishing.

spring-modulith-events/spring-modulith-events-core/src/main/java/org/springframework/modulith/events/config/EventPublicationConfiguration.java
@@ -113,7 +113,7 @@ class EventPublicationConfiguration {
 		private boolean anyPropertyConfigured(String... properties) {
 
 			return Arrays.stream(properties)
-					.map(it -> environment.getProperty(it, (String) null))
+					.map(it -> environment.containsProperty(it))
 					.anyMatch(it -> it != null);
 		}
 	}

commit 45d51dd9d5d6c18736bf4734ada7362a95b5e50c

GH-149 - Tighten test cases for event publication registry auto-configuration.
Properly test for the activation of the auto-configuration as this seems to have become broken with d9e761e.

spring-modulith-events/spring-modulith-events-core/src/main/java/org/springframework/modulith/events/config/EventPublicationConfiguration.java
@@ -113,8 +113,7 @@ class EventPublicationConfiguration {
 		private boolean anyPropertyConfigured(String... properties) {
 
 			return Arrays.stream(properties)
-					.map(it -> environment.containsProperty(it))
-					.anyMatch(it -> it != null);
+					.anyMatch(it -> environment.containsProperty(it));
 		}
 	}
 }

spring-modulith-events/spring-modulith-events-core/src/test/java/org/springframework/modulith/events/config/EventPublicationConfigurationIntegrationTests.java
@@ -48,7 +48,13 @@ class EventPublicationConfigurationIntegrationTests {
 	void registersAsyncTerminationDefaulterByDefault() {
 
 		basicSetup()
-				.run(context -> assertThat(context).hasSingleBean(AsyncPropertiesDefaulter.class));
+				.run(context -> {
+					assertThat(context).hasSingleBean(AsyncPropertiesDefaulter.class);
+
+					expect(Shutdown::isAwaitTermination, true).andThen(
+							expect(Shutdown::getAwaitTerminationPeriod, Duration.ofSeconds(2)))
+							.accept(context);
+				});
 	}
 
 	@Test // GH-149
@@ -63,7 +69,6 @@ class EventPublicationConfigurationIntegrationTests {
 	void doesNotApplyDefaultingIfShutdownTerminationPropertyConfigured() {
 
 		basicSetup()
-				.withConfiguration(AutoConfigurations.of(TaskExecutionAutoConfiguration.class))
 				.withPropertyValues("spring.task.execution.shutdown.await-termination=false")
 				.run(expect(Shutdown::isAwaitTermination, false));
 	}
@@ -72,7 +77,6 @@ class EventPublicationConfigurationIntegrationTests {
 	void doesNotApplyDefaultingIfShutdownTerminationPeriodPropertyConfigured() {
 
 		basicSetup()
-				.withConfiguration(AutoConfigurations.of(TaskExecutionAutoConfiguration.class))
 				.withPropertyValues("spring.task.execution.shutdown.await-termination-period=10m")
 				.run(expect(Shutdown::getAwaitTerminationPeriod, Duration.ofMinutes(10)));
 	}
@@ -88,7 +92,8 @@ class EventPublicationConfigurationIntegrationTests {
 	private ApplicationContextRunner basicSetup() {
 
 		return new ApplicationContextRunner()
-				.withConfiguration(AutoConfigurations.of(EventPublicationConfiguration.class))
+				.withConfiguration(
+						AutoConfigurations.of(EventPublicationConfiguration.class, TaskExecutionAutoConfiguration.class))
 				.withBean(EventPublicationRepository.class, () -> repository);
 	}
 }