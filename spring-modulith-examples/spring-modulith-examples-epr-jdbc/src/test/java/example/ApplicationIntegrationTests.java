594c22a4c83003a7da1b1afc5e38eec3cf31e812
Oliver Drotbohm <odrotbohm@vmware.com>
2023-04-29 12:36:59 +0200

    GH-181 - Create dedicated submodule for examples.
    Move already existing example underneath that. Added examples for JDBC- and MongoDB-based event publication registry setups.

---
+++ spring-modulith-examples/spring-modulith-examples-epr-jdbc/src/test/java/example/ApplicationIntegrationTests.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2022-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example;
+
+import example.inventory.InventoryUpdated;
+import example.order.OrderManagement;
+
+import java.util.Collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.modulith.events.EventPublicationRegistry;
+import org.springframework.modulith.test.EnableScenarios;
+import org.springframework.modulith.test.Scenario;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+/**
+ * Integration test for the overall application.
+ *
+ * @author Oliver Drotbohm
+ */
+@SpringBootTest
+@EnableScenarios
+@Testcontainers(disabledWithoutDocker = true)
+class ApplicationIntegrationTests {
+
+	@Autowired OrderManagement orders;
+	@Autowired EventPublicationRegistry registry;
+
+	@TestConfiguration
+	static class Infrastructure {
+
+		@ServiceConnection
+		@Bean(initMethod = "start", destroyMethod = "stop")
+		PostgreSQLContainer<?> database() {
+			return new PostgreSQLContainer<>("postgres:15.2");
+		}
+	}
+
+	@Test
+	void bootstrapsApplication(Scenario scenario) throws Exception {
+
+		scenario.stimulate(() -> orders.complete())
+				.andWaitForStateChange(() -> registry.findIncompletePublications(), Collection::isEmpty)
+				.andExpect(InventoryUpdated.class)
+				.toArrive();
+	}
+}
