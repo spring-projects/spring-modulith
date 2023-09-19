bf404a571c825cdf4adc8647d09c871fbf819feb
Oliver Drotbohm <odrotbohm@vmware.com>
2023-09-19 11:04:47 +0200

    GH-248 - Add package-info.java for event externalization modules.

---
+++ spring-modulith-events/spring-modulith-events-jms/src/main/java/org/springframework/modulith/events/jms/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * JMS event externalization support.
+ */
+@org.springframework.lang.NonNullApi
+package org.springframework.modulith.events.jms;
---
+++ spring-modulith-events/spring-modulith-events-kafka/src/main/java/org/springframework/modulith/events/kafka/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Kafka event externalization support.
+ */
+@org.springframework.lang.NonNullApi
+package org.springframework.modulith.events.kafka;
