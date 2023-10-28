d95da874bd4d014fb7962ce9865786651948a122
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2023-10-28 12:23:54 +0200

    GH-344 - Support for event externalization into AWS SNS and SQS.
    Additional event externalization implementations for AWS SNS and SQS.
    
    Original pull request: GH-350.

---
+++ spring-modulith-events/spring-modulith-events-aws-sns/src/main/java/org/springframework/modulith/events/aws/sns/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * SNS event externalization support.
+ */
+@org.springframework.lang.NonNullApi
+package org.springframework.modulith.events.aws.sns;
---
+++ spring-modulith-events/spring-modulith-events-aws-sqs/src/main/java/org/springframework/modulith/events/aws/sqs/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * SQS event externalization support.
+ */
+@org.springframework.lang.NonNullApi
+package org.springframework.modulith.events.aws.sqs;
