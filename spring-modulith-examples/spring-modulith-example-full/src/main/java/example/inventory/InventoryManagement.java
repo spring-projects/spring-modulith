0aa101d5ee63ee70b29c7f7b9dadff5a70a439ff
Oliver Drotbohm <oliver.drotbohm@broadcom.com>
2024-01-10 14:46:50 +0100

    GH-444 - Polishing.

--- spring-modulith-examples/spring-modulith-example-full/src/main/java/example/inventory/InventoryManagement.java
+++ spring-modulith-examples/spring-modulith-example-full/src/main/java/example/inventory/InventoryManagement.java
@@ -20,7 +20,7 @@ import lombok.RequiredArgsConstructor;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.modulith.ApplicationModuleListener;
+import org.springframework.modulith.events.ApplicationModuleListener;
 import org.springframework.stereotype.Service;
 
 /**
