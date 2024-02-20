840a1b61b7e5705e389926d4de1a5e7c0883c184
Oliver Drotbohm <oliver.drotbohm@broadcom.com>
2024-02-20 12:53:18 +0100

    GH-403 - Polishing.
    Added indexes for HSQLDB (after fix for GH-507). Removed by-listener index for MySQL as it would not be used anyway. Polished index names. Disable Spring Boot banners in tests.
    
    Original pull request: GH-411.

--- spring-modulith-events/spring-modulith-events-jdbc/src/main/resources/schema-postgresql.sql
+++ spring-modulith-events/spring-modulith-events-jdbc/src/main/resources/schema-postgresql.sql
@@ -8,5 +8,5 @@ CREATE TABLE IF NOT EXISTS event_publication
   completion_date  TIMESTAMP WITH TIME ZONE,
   PRIMARY KEY (id)
 );
-CREATE INDEX IF NOT EXISTS event_publication_listener_id_serialized_event_idx ON event_publication (listener_id, serialized_event);
-CREATE INDEX IF NOT EXISTS event_publication_completion_date_idx ON event_publication (completion_date);
+CREATE INDEX IF NOT EXISTS event_publication_by_listener_id_and_serialized_event_idx ON event_publication (listener_id, serialized_event);
+CREATE INDEX IF NOT EXISTS event_publication_by_completion_date_idx ON event_publication (completion_date);
