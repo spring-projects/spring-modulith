1707be1c54ca980410727052e756cf53799c2c2e
Lukas Dohmen <l.dohmen@yahoo.de>
2024-05-19 11:42:11 +0200

    GH-622 - Polishing in Asciidoctor rendering.

--- spring-modulith-docs/src/main/java/org/springframework/modulith/docs/Documenter.java
+++ spring-modulith-docs/src/main/java/org/springframework/modulith/docs/Documenter.java
@@ -349,7 +349,7 @@ public class Documenter {
 
 				// Properties
 				.append(addTableRow("Properties",
-						asciidoctor.renderConfigurationProperties(module, properties.getModuleProperties(module)), options)) //
+						asciidoctor.renderConfigurationProperties(properties.getModuleProperties(module)), options)) //
 				.append(startOrEndTable())
 				.toString();
 	}
