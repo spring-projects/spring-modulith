6bcc9c4d02b92915020869127001a24a75ef4c20
Josh Long <josh@joshlong.com>
2024-10-12 12:47:58 +0200

    processors

--- spring-modulith-events/spring-modulith-events-core/src/main/java/org/springframework/modulith/events/config/restart/DefaultIncompleteEventPublicationsProcessorConfiguration.java
+++ spring-modulith-events/spring-modulith-events-core/src/main/java/org/springframework/modulith/events/config/restart/DefaultIncompleteEventPublicationsProcessorConfiguration.java
@@ -13,13 +13,12 @@ import org.springframework.modulith.events.support.restart.IncompleteEventPublic
 import java.util.Optional;
 
 /**
- * 
  * Default behavior configured to resubmit all incomplete events.
- * 
- * @author Josh Long 
+ *
+ * @author Josh Long
  */
 @AutoConfiguration
-@AutoConfigureAfter (ExclusiveIncompleteEventPublicationsProcessorConfiguration.class)
+@AutoConfigureAfter(ExclusiveIncompleteEventPublicationsProcessorConfiguration.class)
 class DefaultIncompleteEventPublicationsProcessorConfiguration {
 
     static final String REPUBLISH_ON_RESTART = "spring.modulith.events.republish-outstanding-events-on-restart";
@@ -27,11 +26,11 @@ class DefaultIncompleteEventPublicationsProcessorConfiguration {
     static final String REPUBLISH_ON_RESTART_LEGACY = "spring.modulith.republish-outstanding-events-on-restart";
 
     @Bean
-    @ConditionalOnBean (IncompleteEventPublications.class)
-    @ConditionalOnMissingBean (IncompleteEventPublicationsProcessor.class)
-    DefaultIncompleteEventPublicationsProcessor defaultIncompleteEventPublicationsProcessor(IncompleteEventPublications publications , Environment environment ) {
+    @ConditionalOnBean(IncompleteEventPublications.class)
+    @ConditionalOnMissingBean(IncompleteEventPublicationsProcessor.class)
+    DefaultIncompleteEventPublicationsProcessor defaultIncompleteEventPublicationsProcessor(IncompleteEventPublications publications, Environment environment) {
         var republishOnRestart = Optional.ofNullable(environment.getProperty(REPUBLISH_ON_RESTART, Boolean.class))
-            .orElseGet(() -> environment.getProperty(REPUBLISH_ON_RESTART_LEGACY, Boolean.class));
+                .orElseGet(() -> environment.getProperty(REPUBLISH_ON_RESTART_LEGACY, Boolean.class));
         return new DefaultIncompleteEventPublicationsProcessor(
                 Boolean.TRUE.equals(republishOnRestart), publications);
     }
--- spring-modulith-events/spring-modulith-events-core/src/main/java/org/springframework/modulith/events/support/restart/IncompleteEventPublicationsProcessor.java
+++ spring-modulith-events/spring-modulith-events-core/src/main/java/org/springframework/modulith/events/support/restart/IncompleteEventPublicationsProcessor.java
@@ -2,7 +2,8 @@ package org.springframework.modulith.events.support.restart;
 
 /**
  * Marker interface for components that, on restart, process incomplete event publications
- * @author Josh Long 
+ *
+ * @author Josh Long
  */
 public interface IncompleteEventPublicationsProcessor {
 }
