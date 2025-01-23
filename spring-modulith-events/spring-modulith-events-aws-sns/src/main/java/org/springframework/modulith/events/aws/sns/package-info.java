f3f8deb3b36efd9d854786aef05092cfed71ba21
Oliver Drotbohm <oliver.drotbohm@broadcom.com>
2025-01-23 12:56:36 +0100

    GH-1024 - Remove AWS support in favor of Spring Cloud AWS.
    Follow-up of GH-671.

--- spring-modulith-events/spring-modulith-events-aws-sns/src/main/java/org/springframework/modulith/events/aws/sns/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * AWS SNS event externalization support.
- */
-@org.springframework.lang.NonNullApi
-package org.springframework.modulith.events.aws.sns;
--- spring-modulith-events/spring-modulith-events-aws-sqs/src/main/java/org/springframework/modulith/events/aws/sqs/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * AWS SQS event externalization support.
- */
-@org.springframework.lang.NonNullApi
-package org.springframework.modulith.events.aws.sqs;
