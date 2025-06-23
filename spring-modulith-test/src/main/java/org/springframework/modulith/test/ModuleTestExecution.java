c2af8faf8f7311c4df49302339d2660143ec7a3d
Oliver Drotbohm <oliver.drotbohm@broadcom.com>
2025-06-23 14:48:34 +0200

    GH-1216 - Avoid referring to the source class in ModuleTypeExcludeFilter.equals(…) and ….hashCode().
    MTEF's equals(…) and hashCode() methods is depended on by the application context caching facilities of Spring Framework's test context framework. Previously, it referred to the class under test in those methods, thus effectively disabling the cache, even if the application module test bootstrap setup was identical but used on different test classes.
    
    We now avoid this by solely referring to the equals(…) and hashCode() methods of ModuleTestExecution, which in turn only considers the base package and the @ApplicationModuleTest annotation.

--- spring-modulith-test/src/main/java/org/springframework/modulith/test/ModuleTestExecution.java
+++ spring-modulith-test/src/main/java/org/springframework/modulith/test/ModuleTestExecution.java
@@ -106,7 +106,7 @@ public class ModuleTestExecution implements Iterable<ApplicationModule> {
 
 	public static Supplier<ModuleTestExecution> of(Class<?> type) {
 
-		return () -> {
+		return SingletonSupplier.of(() -> {
 
 			var annotation = AnnotatedElementUtils.findMergedAnnotation(type, ApplicationModuleTest.class);
 
@@ -127,7 +127,7 @@ public class ModuleTestExecution implements Iterable<ApplicationModule> {
 
 			return EXECUTIONS.computeIfAbsent(new Key(module.getBasePackage().getName(), annotation),
 					it -> new ModuleTestExecution(annotation, modules, module));
-		};
+		});
 	}
 
 	/**
