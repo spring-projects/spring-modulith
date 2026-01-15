6dff7514b988ccfd38281efa1fca63606a1f2358
Roland Beisel <info@rolandbeisel.de>
2026-01-15 15:07:39 +0100

    GH-1517 - Add outbox mode for event externalization.
    Signed-off-by: Roland Beisel <info@rolandbeisel.de>

---
+++ spring-modulith-examples/spring-modulith-example-outbox/src/main/java/example/order/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The logical application module order implemented as a multi-package module. Internal components located in nested
+ * packages are prevented from being accessed by the {@link org.springframework.modulith.core.ApplicationModules} type.
+ */
+@org.jspecify.annotations.NullMarked
+package example.order;
---
+++ spring-modulith-examples/spring-modulith-example-outbox/src/test/java/example/TestApplication.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2023-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import example.order.Order;
+import example.order.OrderManagement;
+
+import io.namastack.outbox.annotation.EnableOutbox;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.kafka.core.KafkaOperations;
+import org.springframework.messaging.Message;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author Roland Beisel
+ */
+@EnableOutbox
+@EnableScheduling
+@SpringBootApplication
+public class TestApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(TestApplication.class);
+
+	@Bean
+	@Primary
+	@SuppressWarnings("unchecked")
+	KafkaOperations<?, ?> kafkaOperations() {
+
+		var mock = mock(KafkaOperations.class);
+
+		when(mock.send(any(Message.class))).then(invocation -> {
+
+			logger.info("Sending message {}.", invocation.getArguments()[0]);
+
+			return null;
+		});
+
+		return mock;
+	}
+
+	public static void main(String[] args) {
+
+		var orders = SpringApplication.run(TestApplication.class, args)
+				.getBean(OrderManagement.class);
+
+		logger.info("Triggering order completion…");
+
+		orders.complete(new Order());
+	}
+}
