4aa04bada4f8d5aa305b9d91e8b5c6c935b12acb
Roland Beisel <info@rolandbeisel.de>
2026-01-15 15:07:39 +0100

    GH-1522 - Add outbox mode for event externalization.
    Signed-off-by: Roland Beisel <info@rolandbeisel.de>

---
+++ spring-modulith-events/spring-modulith-events-core/src/main/java/org/springframework/modulith/events/outbox/OutboxEventRecorder.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2023-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.modulith.events.outbox;
+
+import io.namastack.outbox.Outbox;
+
+import org.jspecify.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.PayloadApplicationEvent;
+import org.springframework.modulith.events.EventExternalizationConfiguration;
+import org.springframework.util.Assert;
+
+/**
+ * An {@link ApplicationListener} that listens to all {@link PayloadApplicationEvent}s and schedules
+ * externalized events to an {@link Outbox} for later processing. This listener operates synchronously
+ * within the same transaction as the event publisher, ensuring the outbox record is committed or
+ * rolled back together with the business operation.
+ *
+ * @author Roland Beisel
+ * @since 2.1
+ * @see Outbox
+ * @see EventExternalizationConfiguration
+ */
+class OutboxEventRecorder implements ApplicationListener<PayloadApplicationEvent<?>> {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(OutboxEventRecorder.class);
+
+	private final EventExternalizationConfiguration configuration;
+	private final Outbox outbox;
+
+	/**
+	 * Creates a new {@link OutboxEventRecorder} for the given {@link EventExternalizationConfiguration}
+	 * and {@link Outbox}.
+	 *
+	 * @param configuration must not be {@literal null}.
+	 * @param outbox must not be {@literal null}.
+	 */
+	OutboxEventRecorder(EventExternalizationConfiguration configuration, Outbox outbox) {
+
+		Assert.notNull(configuration, "EventExternalizationConfiguration must not be null!");
+		Assert.notNull(outbox, "Outbox must not be null!");
+
+		this.configuration = configuration;
+		this.outbox = outbox;
+	}
+
+	/**
+	 * Handles incoming application events by checking if they should be externalized
+	 * and scheduling them to the outbox within the current transaction.
+	 *
+	 * @param event the payload application event to process
+	 */
+	@Override
+	public void onApplicationEvent(PayloadApplicationEvent<?> event) {
+
+		var payload = event.getPayload();
+
+		if (!configuration.supports(payload)) {
+			return;
+		}
+
+		var target = configuration.determineTarget(payload);
+		var mapped = configuration.map(payload);
+		var key = target.getKey();
+
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("Scheduling event of type {} to outbox for target {}.",
+					payload.getClass().getName(), target.getTarget());
+		}
+
+		scheduleToOutbox(mapped, key);
+	}
+
+	/**
+	 * Schedules the given payload to the outbox, optionally with a routing key.
+	 *
+	 * @param payload must not be {@literal null}.
+	 * @param key can be {@literal null}.
+	 */
+	private void scheduleToOutbox(Object payload, @Nullable String key) {
+
+		if (key != null) {
+			outbox.schedule(payload, key);
+		} else {
+			outbox.schedule(payload);
+		}
+	}
+}
---
+++ spring-modulith-events/spring-modulith-events-core/src/main/java/org/springframework/modulith/events/outbox/OutboxEventTransport.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2023-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.modulith.events.outbox;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.BiFunction;
+
+import io.namastack.outbox.handler.OutboxHandler;
+import io.namastack.outbox.handler.OutboxRecordMetadata;
+
+import org.springframework.modulith.events.EventExternalizationConfiguration;
+import org.springframework.modulith.events.RoutingTarget;
+import org.springframework.util.Assert;
+
+/**
+ * An {@link OutboxHandler} that transports events from the outbox to an external target.
+ * <p>
+ * This handler is invoked by the outbox processor to externalize events that were
+ * previously recorded in the outbox table. It uses the configured transport function
+ * to deliver events to their target destination (e.g., message broker, email, SMS).
+ *
+ * @author Roland Beisel
+ * @since 2.1
+ * @see OutboxHandler
+ * @see EventExternalizationConfiguration
+ */
+public class OutboxEventTransport implements OutboxHandler {
+
+	private final EventExternalizationConfiguration configuration;
+	private final BiFunction<RoutingTarget, Object, CompletableFuture<?>> transport;
+
+	/**
+	 * Creates a new {@link OutboxEventTransport} for the given {@link EventExternalizationConfiguration}
+	 * and transport function.
+	 *
+	 * @param configuration must not be {@literal null}.
+	 * @param transport must not be {@literal null}.
+	 */
+	public OutboxEventTransport(EventExternalizationConfiguration configuration,
+			BiFunction<RoutingTarget, Object, CompletableFuture<?>> transport) {
+
+		Assert.notNull(configuration, "EventExternalizationConfiguration must not be null!");
+		Assert.notNull(transport, "Transport function must not be null!");
+
+		this.configuration = configuration;
+		this.transport = transport;
+	}
+
+	/**
+	 * Handles an outbox event by determining its routing target and transporting it
+	 * to the external target using the configured transport function.
+	 *
+	 * @param event the event payload to transport
+	 * @param metadata the outbox record metadata
+	 */
+	@Override
+	public void handle(Object event, OutboxRecordMetadata metadata) {
+
+		var target = configuration.determineTarget(event);
+
+		try {
+			transport.apply(target, event).get();
+		} catch (InterruptedException | ExecutionException e) {
+			throw new RuntimeException("Failed to transport event to " + target.getTarget(), e);
+		}
+	}
+}
