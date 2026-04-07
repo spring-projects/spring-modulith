7c2c84df2f47a338bdcb196ac9543b49cf119bee
junietrytwo <junietrytwo@outlook.com>
2026-04-07 17:56:22 +0800

    https://github.com/spring-projects/spring-modulith/issues/1048

---
+++ spring-modulith-maven-plugin/src/test/java/org/springframework/modulith/maven/sample/inventory/InventoryManagement.java
@@ -0,0 +1,12 @@
+package org.springframework.modulith.maven.sample.inventory;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InventoryManagement {
+
+	public String reserve(String item) {
+		return item;
+	}
+}
+
